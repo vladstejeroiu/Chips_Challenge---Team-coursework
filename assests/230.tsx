5bd778b0f9aabe2aed34cd6432cdb795035ec9c0
Vlad Stejeroiu <56177535+vladstejeroiu@users.noreply.github.com>
2020-02-14 19:42:51 +0000

    Add files via upload

---
+++ assests/230.tsx
@@ -0,0 +1,307 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="230" tilewidth="448" tileheight="448" tilecount="101" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="png/000010010.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="png/000011000.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="png/000011010.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="png/000011011.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="png/000110000.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="png/000110010.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="png/000110110.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="png/000111000.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="png/000111010.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="png/000111011.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="png/000111110.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="png/000111111.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="png/010010000.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="png/010010010.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="png/010011000.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="png/010011010.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="png/010011011.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="png/010110000.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="png/010110010.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="png/010110110.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="png/010111000.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="png/010111010.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="png/010111011.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="png/010111110.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="png/010111111.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="png/011011000.png"/>
+ </tile>
+ <tile id="26">
+  <image width="64" height="64" source="png/011011010.png"/>
+ </tile>
+ <tile id="27">
+  <image width="64" height="64" source="png/011011011.png"/>
+ </tile>
+ <tile id="28">
+  <image width="64" height="64" source="png/011111000.png"/>
+ </tile>
+ <tile id="29">
+  <image width="64" height="64" source="png/011111010.png"/>
+ </tile>
+ <tile id="30">
+  <image width="64" height="64" source="png/011111011.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="64" source="png/011111110.png"/>
+ </tile>
+ <tile id="32">
+  <image width="64" height="64" source="png/011111111.png"/>
+ </tile>
+ <tile id="33">
+  <image width="64" height="64" source="png/110110000.png"/>
+ </tile>
+ <tile id="34">
+  <image width="64" height="64" source="png/110110010.png"/>
+ </tile>
+ <tile id="35">
+  <image width="64" height="64" source="png/110110110.png"/>
+ </tile>
+ <tile id="36">
+  <image width="64" height="64" source="png/110111000.png"/>
+ </tile>
+ <tile id="37">
+  <image width="64" height="64" source="png/110111010.png"/>
+ </tile>
+ <tile id="38">
+  <image width="64" height="64" source="png/110111011.png"/>
+ </tile>
+ <tile id="39">
+  <image width="64" height="64" source="png/110111110.png"/>
+ </tile>
+ <tile id="40">
+  <image width="64" height="64" source="png/110111111.png"/>
+ </tile>
+ <tile id="41">
+  <image width="64" height="64" source="png/111111000.png"/>
+ </tile>
+ <tile id="42">
+  <image width="64" height="64" source="png/111111010.png"/>
+ </tile>
+ <tile id="43">
+  <image width="64" height="64" source="png/111111011.png"/>
+ </tile>
+ <tile id="44">
+  <image width="64" height="64" source="png/111111110.png"/>
+ </tile>
+ <tile id="45">
+  <image width="64" height="64" source="png/111111111.png"/>
+ </tile>
+ <tile id="46">
+  <image width="64" height="64" source="png/basic_tree_with_grass.png"/>
+ </tile>
+ <tile id="47">
+  <image width="64" height="64" source="png/blue_axe.png"/>
+ </tile>
+ <tile id="48">
+  <image width="64" height="64" source="png/blue_ghost_left.png"/>
+ </tile>
+ <tile id="49">
+  <image width="128" height="128" source="png/boulder.png"/>
+ </tile>
+ <tile id="50">
+  <image width="64" height="64" source="png/bush_with_grass.png"/>
+ </tile>
+ <tile id="51">
+  <image width="64" height="64" source="png/dynamite.png"/>
+ </tile>
+ <tile id="52">
+  <image width="400" height="200" source="png/dynamite_1.png"/>
+ </tile>
+ <tile id="53">
+  <image width="400" height="200" source="png/dynamite_2.png"/>
+ </tile>
+ <tile id="54">
+  <image width="400" height="200" source="png/dynamite_3.png"/>
+ </tile>
+ <tile id="55">
+  <image width="400" height="200" source="png/dynamite_4.png"/>
+ </tile>
+ <tile id="56">
+  <image width="400" height="200" source="png/dynamite_5.png"/>
+ </tile>
+ <tile id="57">
+  <image width="400" height="200" source="png/dynamite_6.png"/>
+ </tile>
+ <tile id="58">
+  <image width="400" height="200" source="png/dynamite_7.png"/>
+ </tile>
+ <tile id="59">
+  <image width="400" height="200" source="png/dynamite_8.png"/>
+ </tile>
+ <tile id="60">
+  <image width="400" height="200" source="png/dynamite_9.png"/>
+ </tile>
+ <tile id="61">
+  <image width="64" height="64" source="png/e_player.png"/>
+ </tile>
+ <tile id="62">
+  <image width="64" height="64" source="png/flippers.png"/>
+ </tile>
+ <tile id="63">
+  <image width="64" height="64" source="png/flippers_shadow.png"/>
+ </tile>
+ <tile id="64">
+  <image width="64" height="64" source="png/hor_boulder.png"/>
+ </tile>
+ <tile id="65">
+  <image width="64" height="64" source="png/hor_lava.png"/>
+ </tile>
+ <tile id="66">
+  <image width="64" height="64" source="png/hor_log_blue.png"/>
+ </tile>
+ <tile id="67">
+  <image width="64" height="64" source="png/hor_log_purple.png"/>
+ </tile>
+ <tile id="68">
+  <image width="64" height="64" source="png/hor_log_red.png"/>
+ </tile>
+ <tile id="69">
+  <image width="64" height="64" source="png/hor_water.png"/>
+ </tile>
+ <tile id="70">
+  <image width="64" height="64" source="png/lava_charm.png"/>
+ </tile>
+ <tile id="71">
+  <image width="64" height="64" source="png/lava_charm_shadow.png"/>
+ </tile>
+ <tile id="72">
+  <image width="64" height="64" source="png/left_bottom_shroom.png"/>
+ </tile>
+ <tile id="73">
+  <image width="64" height="64" source="png/left_top_shroom.png"/>
+ </tile>
+ <tile id="74">
+  <image width="64" height="64" source="png/n_player.png"/>
+ </tile>
+ <tile id="75">
+  <image width="64" height="64" source="png/pine_tree_with_grass.png"/>
+ </tile>
+ <tile id="76">
+  <image width="64" height="64" source="png/purple_axe.png"/>
+ </tile>
+ <tile id="77">
+  <image width="64" height="64" source="png/red_axe.png"/>
+ </tile>
+ <tile id="78">
+  <image width="64" height="64" source="png/right_bottom_shroom.png"/>
+ </tile>
+ <tile id="79">
+  <image width="64" height="64" source="png/right_top_shroom.png"/>
+ </tile>
+ <tile id="80">
+  <image width="64" height="64" source="png/s_player.png"/>
+ </tile>
+ <tile id="81">
+  <image width="64" height="64" source="png/teleporter.png"/>
+ </tile>
+ <tile id="82">
+  <image width="64" height="64" source="png/test_player.png"/>
+ </tile>
+ <tile id="83">
+  <image width="64" height="64" source="png/ver_boulder.png"/>
+ </tile>
+ <tile id="84">
+  <image width="64" height="64" source="png/ver_lava.png"/>
+ </tile>
+ <tile id="85">
+  <image width="64" height="64" source="png/ver_log_blue.png"/>
+ </tile>
+ <tile id="86">
+  <image width="64" height="64" source="png/ver_log_purple.png"/>
+ </tile>
+ <tile id="87">
+  <image width="64" height="64" source="png/ver_log_red.png"/>
+ </tile>
+ <tile id="88">
+  <image width="64" height="64" source="png/ver_water.png"/>
+ </tile>
+ <tile id="89">
+  <image width="64" height="64" source="png/w_player.png"/>
+ </tile>
+ <tile id="90">
+  <image width="64" height="64" source="png/x0.png"/>
+ </tile>
+ <tile id="91">
+  <image width="64" height="64" source="png/x1.png"/>
+ </tile>
+ <tile id="92">
+  <image width="64" height="64" source="png/x2.png"/>
+ </tile>
+ <tile id="93">
+  <image width="64" height="64" source="png/x3.png"/>
+ </tile>
+ <tile id="94">
+  <image width="64" height="64" source="png/x4.png"/>
+ </tile>
+ <tile id="95">
+  <image width="64" height="64" source="png/x5.png"/>
+ </tile>
+ <tile id="96">
+  <image width="64" height="64" source="png/x6.png"/>
+ </tile>
+ <tile id="97">
+  <image width="64" height="64" source="png/x7.png"/>
+ </tile>
+ <tile id="98">
+  <image width="64" height="64" source="png/x8.png"/>
+ </tile>
+ <tile id="99">
+  <image width="64" height="64" source="png/x9.png"/>
+ </tile>
+ <tile id="100">
+  <image width="448" height="448" source="png/You_Died.png"/>
+ </tile>
+</tileset>
